3cc341a99b8845e5cef0160b20e2a1d22b77c2e2
黄文俊 <huangwj@goldlokedu.cn>
2018-05-07 13:36:39 +0800

    添加模块的package

---
+++ src/main/scala/cn/goldlokedu/alicomp/documents/DubboMethod.scala
@@ -0,0 +1,8 @@
+package cn.goldlokedu.alicomp.documents
+
+case class DubboMethod(interface:String,
+                       method:String,
+                       parameterTypeString:String,
+                       parameter:String)
+
+
