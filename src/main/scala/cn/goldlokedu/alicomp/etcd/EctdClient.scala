07ac3a6fa96dd15b66bd3eddc4c4acde29f82af7
spoofer <2457876902@qq.com>
2018-05-09 18:53:31 +0800

    完成ectd注册发现模块

--- src/main/scala/cn/goldlokedu/alicomp/etcd/EctdClient.scala
+++ src/main/scala/cn/goldlokedu/alicomp/etcd/EctdClient.scala
@@ -4,15 +4,13 @@ import cn.goldlokedu.alicomp.documents.RegisteredAgent
 import org.etcd4s.pb.mvccpb.KeyValue
 import spray.json._
 import org.etcd4s.{Etcd4sClient, Etcd4sClientConfig}
-import org.etcd4s.implicits._
 import org.etcd4s.formats.Formats._
-import cn.goldlokedu.alicomp.util.json.ServiceProtocol._
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class EctdClient(host: String, port: Int)(implicit dispatcher: ExecutionContext) {
   private val providerPath = "providers/"
-  private val consumerpath = "consumers"
+  private val consumerPath = "consumers/"
   private val client = Etcd4sClient.newClient(Etcd4sClientConfig(address = host, port = port))
 
   def addProvider(agent: RegisteredAgent): Future[Option[KeyValue]] = {
@@ -21,6 +19,18 @@ class EctdClient(host: String, port: Int)(implicit dispatcher: ExecutionContext)
 
   def providers(): Future[Seq[RegisteredAgent]] = {
     client.kvService.getRange(providerPath) map { elem =>
+      elem.kvs.map { kv =>
+        kv.value.toStringUtf8.parseJson.convertTo[RegisteredAgent]
+      }
+    }
+  }
+
+  def addConsumers(agent: RegisteredAgent): Future[Option[KeyValue]] = {
+    client.kvService.setKey(s"$consumerPath${agent.agentName}", agent.toJson.compactPrint)
+  }
+
+  def consumers(): Future[Seq[RegisteredAgent]] = {
+    client.kvService.getRange(consumerPath) map { elem =>
       elem.kvs.map { kv =>
         kv.value.toStringUtf8.parseJson.convertTo[RegisteredAgent]
       }
