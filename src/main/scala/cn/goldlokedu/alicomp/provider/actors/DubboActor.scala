cf957b1b633bb61b20df85a398a753f81eb804d3
黄文俊 <huangwj@goldlokedu.cn>
2018-05-20 09:15:15 +0800

    修复调整

--- src/main/scala/cn/goldlokedu/alicomp/provider/actors/DubboActor.scala
+++ src/main/scala/cn/goldlokedu/alicomp/provider/actors/DubboActor.scala
@@ -55,8 +55,8 @@ class DubboActor(dubboHost: String,
       connection = Some(sender())
       connection.get ! Register(self)
       // debug
-            implicit val ec = context.dispatcher
-            context.system.scheduler.schedule(1 second, 1 second, self, PrintPayload)
+//            implicit val ec = context.dispatcher
+//            context.system.scheduler.schedule(1 second, 1 second, self, PrintPayload)
 
       context become ready
   }
