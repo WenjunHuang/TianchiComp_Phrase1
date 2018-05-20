d4bd2c2ee8aaec27b862041e347314766db05fa8
黄文俊 <huangwj@goldlokedu.cn>
2018-05-20 18:53:08 +0800

    调整负载均衡

--- src/main/scala/cn/goldlokedu/alicomp/provider/actors/DubboActor.scala
+++ src/main/scala/cn/goldlokedu/alicomp/provider/actors/DubboActor.scala
@@ -57,9 +57,9 @@ class DubboActor(dubboHost: String,
       connection = Some(sender())
       connection.get ! Register(self)
       // debug
-      //      implicit val ec = context.dispatcher
-      //      context.system.scheduler.schedule(1 second, 1 second, self, PrintPayload)
-      //            context.system.scheduler.schedule(1 second, 100 milliseconds, self, TrySend)
+            implicit val ec = context.dispatcher
+            context.system.scheduler.schedule(1 second, 1 second, self, PrintPayload)
+                  context.system.scheduler.schedule(1 second, 100 milliseconds, self, TrySend)
 
       context become ready
   }
