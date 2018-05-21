dd48cdc5734059f45f96a5d3e44dec3ae912feab
黄文俊 <huangwj@goldlokedu.cn>
2018-05-21 18:56:06 +0800

    换dispatcher

--- src/main/scala/cn/goldlokedu/alicomp/consumer/actors/ConsumerAgentActor.scala
+++ src/main/scala/cn/goldlokedu/alicomp/consumer/actors/ConsumerAgentActor.scala
@@ -87,9 +87,9 @@ class ConsumerAgentActor(etcdClient: => EtcdClient)(implicit ec: ExecutionContex
     val cap = roll match {
       case 0 =>
         CapacityType.S
-      case x if (1 to 3) contains x =>
+      case x if (1 to 2) contains x =>
         CapacityType.M
-      case x if (4 to 9) contains x =>
+      case x if (3 to 9) contains x =>
         CapacityType.L
       case _ =>
         CapacityType.L
