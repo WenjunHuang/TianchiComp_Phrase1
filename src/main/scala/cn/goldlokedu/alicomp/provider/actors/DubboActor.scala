a363dd9f3441ef0d70f92c3481d082ea9ddd92c6
WenjunHuang <huangwj@goldlokedu.cn>
2018-05-25 00:46:50 +0800

    使用缺省throughput

--- src/main/scala/cn/goldlokedu/alicomp/provider/actors/DubboActor.scala
+++ src/main/scala/cn/goldlokedu/alicomp/provider/actors/DubboActor.scala
@@ -57,8 +57,8 @@ class DubboActor(dubboHost: String,
       connection = Some(sender())
       connection.get ! Register(self)
       // debug
-      //      implicit val ec = context.dispatcher
-      //      context.system.scheduler.schedule(1 second, 1 second, self, PrintPayload)
+            implicit val ec = context.dispatcher
+            context.system.scheduler.schedule(1 second, 1 second, self, PrintPayload)
       //                  context.system.scheduler.schedule(1 second, 100 milliseconds, self, TrySend)
 
       context become ready
