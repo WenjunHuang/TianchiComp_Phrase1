7f4a730d25da7543aefa58addbd839e15fc625d8
黄文俊 <huangwj@goldlokedu.cn>
2018-06-09 13:48:36 +0800

    去掉日志

--- src/main/scala/cn/goldlokedu/alicomp/documents/BenchmarkResponse.scala
+++ src/main/scala/cn/goldlokedu/alicomp/documents/BenchmarkResponse.scala
@@ -17,7 +17,6 @@ object BenchmarkResponse {
   val KEEP_ALIVE = AsciiString.cached("keep-alive")
 
   def toHttpResponse(message: ByteBuf): FullHttpResponse = {
-    println(ByteBufUtil.hexDump(message))
     // 测试的结果是个32位整数值，但是dubbo用fastjson编码后得出的是一个字符串，例如 9900，结果是"1\n9900\n"字符串
     val status = DubboMessage.extractStatus(message)
     // 头两个字节是"1\n",最后一个字节是"\n",todo 如果是windows，那么是\n\r
