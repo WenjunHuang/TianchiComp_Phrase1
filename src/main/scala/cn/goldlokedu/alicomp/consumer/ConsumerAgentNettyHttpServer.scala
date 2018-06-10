0669562ffc20edaa891821a1aafcacfca96efaa6
黄文俊 <huangwj@goldlokedu.cn>
2018-06-10 22:25:10 +0800

    调优

--- src/main/scala/cn/goldlokedu/alicomp/consumer/ConsumerAgentNettyHttpServer.scala
+++ src/main/scala/cn/goldlokedu/alicomp/consumer/ConsumerAgentNettyHttpServer.scala
@@ -36,7 +36,7 @@ class ConsumerAgentNettyHttpServer(etcdClient: EtcdClient,
         val rest = ras.filterNot(p => providerAgents.contains(p.cap))
         rest.foreach { agent =>
           println(s"connecting $agent")
-          (0 until 4).foreach { _ =>
+          (0 until 2).foreach { _ =>
             val b1 = createProviderAgentChannel
             b1.connect(new InetSocketAddress(agent.host, agent.port))
               .addListener { future: ChannelFuture =>
