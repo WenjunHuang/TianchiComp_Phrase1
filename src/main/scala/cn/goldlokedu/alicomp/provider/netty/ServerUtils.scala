ed33e4720e24dd9ac4ff7426e04f7729cc34e767
黄文俊 <huangwj@goldlokedu.cn>
2018-06-14 17:58:18 +0800

    调优

--- src/main/scala/cn/goldlokedu/alicomp/provider/netty/ServerUtils.scala
+++ src/main/scala/cn/goldlokedu/alicomp/provider/netty/ServerUtils.scala
@@ -2,10 +2,10 @@ package cn.goldlokedu.alicomp.provider.netty
 
 import io.netty.bootstrap.{Bootstrap, ServerBootstrap}
 import io.netty.buffer.Unpooled
-import io.netty.channel.epoll.{Epoll, EpollEventLoopGroup, EpollServerSocketChannel, EpollSocketChannel}
+import io.netty.channel.epoll._
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.{NioServerSocketChannel, NioSocketChannel}
-import io.netty.channel.{Channel, ChannelFutureListener}
+import io.netty.channel.{Channel, ChannelFutureListener, ChannelOption}
 
 object ServerUtils {
   def closeOnFlush(ch: Channel) = {
@@ -14,28 +14,35 @@ object ServerUtils {
   }
 
   def setServerChannelClass(bootstrap: ServerBootstrap) = {
-    if (Epoll.isAvailable())
+    if (Epoll.isAvailable()) {
+      bootstrap.option(ChannelOption.SO_REUSEADDR, true)
       bootstrap.channel(classOf[EpollServerSocketChannel])
+      bootstrap.option[java.lang.Integer](ChannelOption.IP_TOS, 13)
+    }
     else
       bootstrap.channel(classOf[NioServerSocketChannel])
   }
 
   def setChannelClass(bootstrap: Bootstrap) = {
-    if (Epoll.isAvailable())
+    if (Epoll.isAvailable()) {
       bootstrap.channel(classOf[EpollSocketChannel])
-    else
+      bootstrap.option(ChannelOption.SO_REUSEADDR, true)
+      bootstrap.option[java.lang.Integer](ChannelOption.IP_TOS, 13)
+    }
+    else {
       bootstrap.channel(classOf[NioSocketChannel])
+    }
   }
 
   def newGroup(threads: Int = 0) = {
     if (Epoll.isAvailable()) {
       val eg = new EpollEventLoopGroup(threads)
-//      eg.setIoRatio(100)
+      //      eg.setIoRatio(100)
       eg
     }
     else {
       val ng = new NioEventLoopGroup(threads)
-//      ng.setIoRatio(100)
+      //      ng.setIoRatio(100)
       ng
     }
   }
