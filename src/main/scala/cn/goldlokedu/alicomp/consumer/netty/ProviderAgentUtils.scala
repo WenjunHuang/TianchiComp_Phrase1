b48b7cf3d9a5e539a67ebd0a4d8531697ec5e441
黄文俊 <huangwj@goldlokedu.cn>
2018-06-16 14:31:14 +0800

    调优

--- src/main/scala/cn/goldlokedu/alicomp/consumer/netty/ProviderAgentUtils.scala
+++ src/main/scala/cn/goldlokedu/alicomp/consumer/netty/ProviderAgentUtils.scala
@@ -7,11 +7,12 @@ import io.netty.util.concurrent.FastThreadLocal
 
 object ProviderAgentUtils {
   val MaxRoll = 13
-  //  val largeBound = Set(0, 1, 3, 4, 6, 7, 9, 10, 12)
-  val largeBound = Set(0, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12)
-  //  val mediumBound = Set(2, 5, 8, 11)
-  val mediumBound = Nil
-  val smallBound = Nil
+  val largeBound = Set(1, 3, 4, 6, 9, 10, 12)
+  //  val largeBound = Set(0, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12)
+  val mediumBound = Set(2, 5, 8, 11)
+  //  val mediumBound = Nil
+  //  val smallBound = Nil
+  val smallBound = Set(0, 7)
 
   def setProviderAgentChannel(cap: CapacityType.Value, channel: Channel) = {
     cap match {
